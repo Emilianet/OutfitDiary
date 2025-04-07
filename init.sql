807e5275255c322ca487b36616f3959f4e0ee59c
Emilia Bukovski <emmi.bukovski@gmail.com>
2025-04-07 13:25:19 +0300

    Store classes in database

---
+++ init.sql
@@ -0,0 +1,11 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Osasto', 'vapaa-aika');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'urheilu');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'juhla');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'toimisto');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'koulu');
+
+INSERT INTO classes (title, value) VALUES ('Kausi', 'kesä');
+INSERT INTO classes (title, value) VALUES ('Kausi', 'talvi');
+INSERT INTO classes (title, value) VALUES ('Kausi', 'välikausi');
