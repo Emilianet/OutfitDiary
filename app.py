33f2867e15d5b185e32309177241c537b7f4e483
Emilia Bukovski <emmi.bukovski@gmail.com>
2025-05-02 14:09:38 +0300

    Fix import order

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import secrets
 import sqlite3
+
 from flask import Flask
 from flask import abort, flash, make_response, redirect, render_template, request, session
+import markupsafe
+
 import config
 import db
 import items
 import users
-import markupsafe
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
